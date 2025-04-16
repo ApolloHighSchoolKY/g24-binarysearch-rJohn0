084a078c6fcc277e45b69b9989c4c2a198801d98
rJohn0 <romeo.john@stu.daviess.kyschools.us>
2025-04-16 17:11:44 +0000

    everything but last print

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,29 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
-
+		Arrays.sort(ray);
+		int top = ray.length-1;
+		int bottom = ray[0];
+		int middle =0;
+	
+		while (bottom < top) 
+		{
+			middle = (top + bottom) / 2;
+			
+			if(ray[middle] > item)
+			{
+				top = middle;
+			}
+			if(ray[middle] < item)
+			{
+				bottom = middle; 
+			}
+			if(ray[middle] == item)
+			{
+				return middle;
+			}
+			
+		}
+		return -1;
 	}
 }
